0194b9f06d877cfcf2909c2d07f5c0467c0d1602
mirabelli11 <diego.mirabelli@gmail.com>
2023-01-31 18:03:05 -0300

    Formato y scripts

---
+++ scripts.js
@@ -0,0 +1,24 @@
+document.getElementById('verMasABC').addEventListener('click', function(){
+    if(document.getElementById('TareasABC').style.display == '' ||
+    document.getElementById('TareasABC').style.display == 'none'){
+        document.getElementById('TareasABC').style.display = 'block';
+    } else {
+        document.getElementById('TareasABC').style.display = 'none';
+    }
+})
+document.getElementById('verMasXYZ').addEventListener('click', function(){
+    if(document.getElementById('TareasXYZ').style.display == '' ||
+    document.getElementById('TareasXYZ').style.display == 'none'){
+        document.getElementById('TareasXYZ').style.display = 'block';
+    } else {
+        document.getElementById('TareasXYZ').style.display = 'none';
+    }
+})
+document.getElementById('verMas123').addEventListener('click', function(){
+    if(document.getElementById('Tareas123').style.display == '' ||
+    document.getElementById('Tareas123').style.display == 'none'){
+        document.getElementById('Tareas123').style.display = 'block';
+    } else {
+        document.getElementById('Tareas123').style.display = 'none';
+    }
+})
